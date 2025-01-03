d47e992ae21ad66acc8674caa0f467ec9f6e43a2
Meow <108517390+MeowMeow0417@users.noreply.github.com>
2025-01-03 18:28:34 +0800

    simple change

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2 font-bold text-[8rem]">
-      Putangina mo Gilbert
+      Putangina mo Gilbert, ( murit )
     </div>
   );
 }
